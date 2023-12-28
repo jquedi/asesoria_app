55debdc5b2ee7773467210456ac94b5ea5dcc528
jquedi <70477114+jquedi@users.noreply.github.com>
2023-12-28 13:23:55 +0000

    Add import for procesarFactura component

--- src/components/inicio.vue
+++ src/components/inicio.vue
@@ -5,27 +5,35 @@
       <utilidad class="utilidad" msg="todo_list" />
       <utilidad class="utilidad" msg="calendario" />
       <utilidad class="utilidad" msg="clientes" @click="clientesHidden = 'mostrar'" />
-      <utilidad class="utilidad" id="notasDiv" msg="notas" />
+      <utilidad class="utilidad" id="notasDiv" msg="notas"  @click="procesarFacturasHidden = 'mostrar'"/>
       
       <utilidad class="utilidad" msg="archivo" />
 
       <clientes :classValue="clientesHidden" @cerrar="clientesHidden = 'esconder'" />
+
+
+
+      <procesarFacturas v-if="procesarFacturasHidden == 'mostrar'" @cerrar="procesarFacturasHidden = 'esconder'"/>
+
     </div>
   </header>
 </template>
 <script>
 import utilidad from './utilidad.vue'
 import clientes from './tools/clientesTools.vue'
+import procesarFacturas from './tools/toolsComponents/clientes/clienteToolsProcesarFactura.vue'
 export default {
     components: {
       utilidad,
-      clientes
+      clientes,
+      procesarFacturas
     },
     props: {
     },
     data() {
         return {
-          clientesHidden: 'mostrar',
+          clientesHidden: 'esconder',
+          procesarFacturasHidden: 'mostrar'
         }
     },
     watch: {
---
+++ src/components/tools/toolsComponents/clientes/clienteToolsProcesarFactura.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="procesarFacDiv">
+        <botonCerrar @cerrar="cerrar" />
+        <div class="divFactura" v-on:mouseenter="focusForm(false)" v-on:mouseleave="focusForm(true)">
+            <button class="botonAumentar" @click="zoom(50)">+</button>
+            <button class="botonReducir" @click="zoom(-50)">-</button>
+            <img style="height: 100%;left: 0%;top: 0%;" class="imgProcesarFac" src="@/assets/iconos/ejemploFac.jpeg">
+        </div>
+        
+        <div class="divFormProcesarFac"></div>
+    </div>
+</template>
+<script>
+import botonCerrar from '@/components/comunes/botonCerrar.vue';
+export default {
+    components: {
+        botonCerrar
+    },
+    props: {
+    },
+    data() {
+        return {
+        }
+    },
+    watch: {
+    },
+    methods: {
+        cerrar(){
+            this.$emit('cerrar');
+        },
+        zoom(valor){
+            let img = document.getElementsByClassName('imgProcesarFac')[0];
+            let height = parseInt(img.style.height);
+            if((height + valor) > 100){
+                img.style.height = (height + valor) + '%';
+            } else {
+                img.style.height = '100%';
+            }
+        },
+        focusForm(focus){
+            let form = document.getElementsByClassName('divFormProcesarFac')[0];
+            if(focus){
+                form.style.opacity = 1;
+            } else {
+                form.style.opacity = 0.3;
+            }
+        }
+    },
+    mounted() {
+    },
+    created(){
+    }
+}
+</script>
+
+<style scoped>
+</style>
