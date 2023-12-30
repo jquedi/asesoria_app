783e99bb72aeeaabd1c0216278f0ec1f7f3fc036
jquedi <70477114+jquedi@users.noreply.github.com>
2023-12-30 21:14:03 +0000

    Subir PDFs en formato imagenes

--- src/components/tools/toolsComponents/clientes/clienteToolsInfo.vue
+++ src/components/tools/toolsComponents/clientes/clienteToolsInfo.vue
@@ -38,7 +38,7 @@
                         <div class="miniaturaFacturaDato">
                             Proveedor: <strong>{{ factura.proveedor }}</strong>
                         </div>
-                        <div class="miniaturaFacturaDato">
+                        <div class="miniaturaFacturaDato">https://animeflv.net/
                             Fecha: <strong>{{ factura.fecha }}</strong>
                         </div>
                         <div class="miniaturaFacturaDato">
@@ -53,6 +53,16 @@
     </div>
 </template>
 <script>
+// import { getDocument } from 'pdfjs-dist/legacy/build/pdf';
+// import pdfjsWorker from 'pdfjs-dist/legacy/build/pdf.worker';
+
+// // Configurar el workerSrc globalmente
+// pdfjs.GlobalWorkerOptions.workerSrc = pdfjsWorker;
+
+
+
+import html2canvas from 'html2canvas';
+
 import { db } from '/workspaces/asesoria_app/src/firebase.js';
 import { getDownloadURL, ref as storageRef, uploadBytes } from 'firebase/storage';
 import { storage } from '/workspaces/asesoria_app/src/firebase.js'; // Asegúrate de exportar 'storage' desde tu archivo de configuración de Firebase.
@@ -180,30 +190,79 @@ export default {
         },
         async subirArchivos() {
             for (let i = 0; i < this.archivosSeleccionados.length; i++) {
-                const file = this.archivosSeleccionados[i];
-                const uniqueIdentifier = `${this.cliente.nif}-${Date.now()}-${i}`; // Identificador único
-                const storagePath = `facturas/${uniqueIdentifier}`; // Ruta con el identificador único
+                    const file = this.archivosSeleccionados[i];
+                    const uniqueIdentifier = `${this.cliente.nif}-${Date.now()}-${i}`; // Identificador único
+                    const storagePath = `facturas/${uniqueIdentifier}`; // Ruta con el identificador único
+                    const fileRef = storageRef(storage, storagePath);
 
-                const fileRef = storageRef(storage, storagePath);
+                    const datos = this.modeloFac;
+                    
+                if(file.type.startsWith('image/')){
+                    datos.procesado = false;
+                    datos.cliente = this.cliente.nif;
+                    datos.img = [uniqueIdentifier];
 
-                const datos = this.modeloFac;
-                datos.procesado = false;
-                datos.cliente = this.cliente.nif;
-                datos.img = [uniqueIdentifier];
+                    await this.subirImagen(file, fileRef, uniqueIdentifier);
+                    await this.subirDatos(datos, uniqueIdentifier);
 
-                try {
-                const snapshot = await uploadBytes(fileRef, file);
-                console.log(`Archivo ${file.name} subido con éxito como ${uniqueIdentifier}`);
-                const docRef = doc(db, "facturas", uniqueIdentifier);
-                await setDoc(docRef, datos);
-                console.log(`Documento ${uniqueIdentifier} creado con éxito`);
-                } catch (error) {
-                console.error(`Error subiendo el archivo ${file.name}:`, error);
+                }else if(file.type === 'application/pdf'){
+                    // FUNCION PARA CONVERTIR UN PDF A IMAGENES
+                    const nombresImagenes = await this.convertirYSubirPDF(file, uniqueIdentifier, fileRef);
+                    datos.procesado = false;
+                    datos.cliente = this.cliente.nif;
+                    datos.img = nombresImagenes;
+
+                    await this.subirDatos(datos, uniqueIdentifier);
+
+                }else{
+                    console.log('Archivo no válido');
                 }
             }
             // Limpiar la selección después de la carga
             this.archivosSeleccionados = [];
         },
+
+        async convertirYSubirPDF(pdfFile, uniqueIdentifier, fileRef) {
+            let nombresImagenes = [];
+            try {
+                const pdf = await getDocument({ url: URL.createObjectURL(pdfFile) }).promise;
+                for (let pageNum = 1; pageNum <= pdf.numPages; pageNum++) {
+                    const page = await pdf.getPage(pageNum);
+                    const viewport = page.getViewport({ scale: 1 });
+                    const canvas = document.createElement('canvas');
+                    canvas.height = viewport.height;
+                    canvas.width = viewport.width;
+                    await page.render({ canvasContext: canvas.getContext('2d'), viewport }).promise;
+                    
+                    const image = await html2canvas(canvas);
+                    const imageBlob = await new Promise(resolve => image.toBlob(resolve));
+
+                    const imageName = `${uniqueIdentifier}-page${pageNum}`;
+                    await this.subirImagen(imageBlob, fileRef, imageName);
+                    nombresImagenes.push(imageName);
+                }
+            } catch (error) {
+                console.error('Error al convertir el PDF:', error);
+            }
+            return nombresImagenes;
+        },
+        async subirDatos(datos, uniqueIdentifier) {
+            try {
+                const docRef = doc(db, "facturas", uniqueIdentifier);
+                await setDoc(docRef, datos);
+                console.log(`Documento ${uniqueIdentifier} creado con éxito`);
+            } catch (error) {
+                console.error(`Error subiendo el archivo ${file.name}:`, error);
+            }
+        },
+        async subirImagen(file, fileRef, uniqueIdentifier) {
+            try {
+                const snapshot = await uploadBytes(fileRef, file);
+                console.log(`Archivo ${file.name} subido con éxito como ${uniqueIdentifier}`);
+            } catch (error) {
+                console.error(`Error subiendo el archivo ${file.name}:`, error);
+            }
+        }
     },
     mounted() {
         this.listaFacts();
